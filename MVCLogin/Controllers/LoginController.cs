0f2d0e2023553571654b51ae30f67f7775b13cd0
alucard1792 <dz9217@hotmail.com>
2018-02-17 21:10:46 -0500

    Login complete and user validation

---
+++ MVCLogin/Controllers/LoginController.cs
@@ -0,0 +1,46 @@
+﻿using MVCLogin.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCLogin.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Autherize(MVCLogin.Models.Customer customer)
+        {
+            using (mMvcCrudEntities db = new mMvcCrudEntities())
+            {
+                var customerDetail = db.Customers.Where(x => x.Name == customer.Name && x.Description == customer.Description).FirstOrDefault();
+                if (customerDetail == null)
+                {
+                    customer.LoginErrorMessage = "wrong Username or password.";
+                    return View("Index", customer);
+                }
+                else
+                {
+                    Session["Id"] = customer.Id;
+                    Session["Name"] = customer.Name;
+                    Session["Description"] = customer.Description;
+                    return RedirectToAction("Index", "Home");//redireccionamos a otra vista, primer parametro es el metodo home y el segundo es el nombre del controlador
+                }
+            }
+        }
+
+        public ActionResult LogOut()
+        {
+            Session.Abandon();
+            return RedirectToAction("Index", "Login");
+        }
+
+    }
+}
